ad716cc75a30e8aeb96957f884f4dda0792159dc
ErickFPCruz <erick.cruz97@hotmail.com>
2022-05-20 22:17:27 -0300

    Sequência com número finito de iterações terminada.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int nrLimiteComeco, nrLimiteFinal;
+
+Console.Write("Escreva o número do começo: ");
+nrLimiteComeco = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Escreva o número do final: ");
+nrLimiteFinal = Convert.ToInt32(Console.ReadLine()!);
+
+if (nrLimiteComeco > nrLimiteFinal)
+{
+    Console.WriteLine("\nA sequência foi programada para ser crescente!");
+}
+
+else
+{
+
+    while (nrLimiteComeco <= nrLimiteFinal)
+    {
+        Console.Write($"{nrLimiteComeco} ");
+        nrLimiteComeco++;
+    }
+}
+Console.WriteLine();
